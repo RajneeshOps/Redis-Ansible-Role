b2fc116181283ccb825a319e4087e97a2494e8ec
Rajneesh Yadav <112567397+RajneeshOps@users.noreply.github.com>
2024-07-22 17:49:21 +0530

    Update deployRedis.groovy

--- vars/deployRedis.groovy
+++ vars/deployRedis.groovy
@@ -1,20 +1,15 @@
 def call() {
     // Clone the repository
-    git branch: 'main', url: 'https://github.com/Sanchit2323/Redis-ansible.git'
+    git branch: 'main', url: 'https://github.com/RajneeshOps/Redis-Ansible-Role.git'
 
     // User Approval
-    input message: 'Approve the deployment?', submitter: 'admin,sanchit'
+    input message: 'Approve the deployment?', submitter: 'admin,rajneesh'
 
     // Playbook Execution
     ansiblePlaybook(
         inventory: 'aws_ec2.yml',
-        playbook: 'replicated.yml'
+        playbook: 'install.yml'
     )
 
-    // Notification
-    emailext(
-        subject: 'Ansible Deployment Status',
-        body: 'The Ansible deployment has completed.',
-        to: 'sanchitkumar0307@gmail.com'
-    )
+   
 }
