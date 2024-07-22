d8dccdebaa147d3421c8bcabc8eb5e9a6ac64859
Rajneesh Yadav <112567397+RajneeshOps@users.noreply.github.com>
2024-07-22 18:18:33 +0530

    Create deploy.groovy

---
+++ src/org/deploy.groovy
@@ -0,0 +1,14 @@
+def call() {
+    // Clone the repository
+    git branch: 'main', url: 'https://github.com/RajneeshOps/Redis-Ansible-Role.git'
+
+    // User Approval
+    input message: 'Approve the deployment?', submitter: 'admin,rajneesh'
+
+    // Playbook Execution
+    ansiblePlaybook(
+        inventory: 'aws_ec2.yml',
+        playbook: 'install.yml'
+    )
+
+}
