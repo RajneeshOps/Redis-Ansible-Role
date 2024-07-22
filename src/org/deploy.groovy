1f2a29337941580de25e3ea8fd1a9be5988a8b5f
Rajneesh Yadav <112567397+RajneeshOps@users.noreply.github.com>
2024-07-22 18:26:56 +0530

    Update deploy.groovy

--- src/org/deploy.groovy
+++ src/org/deploy.groovy
@@ -1,14 +1,5 @@
+package org.p9
+
 def call() {
-    // Clone the repository
-    git branch: 'main', url: 'https://github.com/RajneeshOps/Redis-Ansible-Role.git'
-
-    // User Approval
-    input message: 'Approve the deployment?', submitter: 'admin,rajneesh'
-
-    // Playbook Execution
-    ansiblePlaybook(
-        inventory: 'aws_ec2.yml',
-        playbook: 'install.yml'
-    )
-
+    sh 'ansible-playbook -i aws_ec2.yml --private-key=/Users/rajneeshyadav/Downloads/sprint4.pem install.yml'
 }
